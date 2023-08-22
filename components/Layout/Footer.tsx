e4fc92663da7791dab4fe42e37e415f4cb76f515
daiweilam <dwb.blue@gmail.com>
2023-08-22 20:56:11 +0800

    update footer and add google font

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,7 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import Button from "./Button";
 import styles from "@/styles/Layout/Footer.module.scss";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.menu}>
+        <div className={styles.list}>
+          <div>Share Knowledge</div>
+          <Link href="#">Open Course</Link>
+        </div>
+        <div className={styles.list}>
+          <div>Get Knowledge</div>
+          <Link href="#">Find Course</Link>
+          <Link href="#">Article</Link>
+        </div>
+      </div>
+
+      <img src="/footer_image.png" alt="" />
+
+      <div className={styles.side}>
+        <div className={styles.top}>
+          <div className={styles.social}>
+            <Link href="#">
+              <i className="bx bxl-facebook" />
+            </Link>
+            <Link href="#">
+              <i className="bx bxl-twitter" />
+            </Link>
+            <Link href="#">
+              <i className="bx bxl-linkedin" />
+            </Link>
+            <Link href="#">
+              <i className="bx bxl-youtube" />
+            </Link>
+          </div>
+          <Link href="/">
+            <div className={styles.logo}>
+              <div>
+                Knock Knowledge<span>.</span>
+              </div>
+              <i className="bx bxs-door-open" />
+            </div>
+          </Link>
+        </div>
+        <div className={styles.bottom}>
+          <Button>
+            TOP <i className="bx bx-up-arrow-alt" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
-import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout/Layout";
+import { DM_Sans } from "next/font/google";
+import "@/styles/globals.scss";
+
+const dm_sans = DM_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <style jsx global>{`
+        html {
+          font-family: ${dm_sans.style.fontFamily};
+        }
+      `}</style>
+    </>
   );
 }
